aeaa1f3b7c5f464ff5ed8453d8c44a086f841d47
Laboratorio <laboratorio@udla-ec.int>
2026-04-28 11:51:18 -0500

    sefundo comit

--- cabezeras.c
+++ cabezeras.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
+#include"sumar.h"
 int main( int argc, char const  *argv[])
 {
 
     int a,b;
-    printf("ingrese 1er numero ")
-
-
-
+    printf("ingrese 1er numero ");
+    scanf("%d",&a);
+    printf("ingrese 2do numero  ");
+    scanf("%d",&b);
+    sumar (a,b);
+    printf("la suma es : %d",sumar (a,b));
+    return 0 ;
 
 
 
