f1893e3e33f81b20e3eaa3ddefb232fc63191479
andytemate <108159622+andytemate@users.noreply.github.com>
2026-04-28 19:39:01 -0500

    cuarto commit

--- cabezeras.c
+++ cabezeras.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include"sumar.h"
+#include"restar.h"
+#include"multiplicar.h"
+#include"dividir.h"
 int main( int argc, char const  *argv[])
 {
 
@@ -9,7 +12,13 @@ int main( int argc, char const  *argv[])
     printf("ingrese 2do numero  ");
     scanf("%d",&b);
     sumar (a,b);
+    restar (a,b);
+    multiplicar (a,b);
+    dividir (a,b);
     printf("la suma es : %d",sumar (a,b));
+    printf("la resta es : %d",restar (a,b));
+    printf("la multiplicacion es : %d",multiplicar (a,b));
+    printf("la division es : %d",dividir (a,b));
     return 0 ;
 
 
